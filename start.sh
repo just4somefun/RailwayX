bbefd61deb3d578c263a11e2116a0a0b7bfe9453
just4somefun <just4somefun@users.noreply.github.com>
2022-04-05 20:56:13 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # configs
-AUUID=f315b7ca-a707-4b9f-a70c-d31a7a386205
+AUUID=$AUUID
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
